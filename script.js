6f04e4863a51067723afb927b4ab8d5aad524696
Yash Bhawsar <104437573+YashBhawsar23@users.noreply.github.com>
2024-05-10 16:55:57 +0530

    Add files via upload
    Made a little bit changes in UI

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
+
 const apiKey = '73254cae6af1a4e2c8d8636608d498b3';
-const apiUrl =  `https://api.openweathermap.org/data/2.5/weather?units=metric&q=khargone`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 
-const searchBox = document.querySelector('.search input')
-const searchBtn = document.querySelector('.search button')
-const weatherIcon = document.querySelector('.weather-icon')
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if(response.status ==  404) {
-        document.querySelector('.error').style.display = 'block'
-        document.querySelector('.weather').style.display = 'none'
+    if (response.status === 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
     } else {
+        const data = await response.json();
 
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+        document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+
+        const weatherMain = data.weather[0].main;
+        switch (weatherMain) {
+            case 'Clouds':
+                weatherIcon.src = 'images/clouds.png';
+                break;
+            case 'Clear':
+                weatherIcon.src = 'images/clear.png';
+                break;
+            case 'Rain':
+                weatherIcon.src = 'images/rain.png';
+                break;
+            case 'Drizzle':
+                weatherIcon.src = 'images/drizzle.png';
+                break;
+            case 'Mist':
+                weatherIcon.src = 'images/mist.png';
+                break;
+            default:
+                weatherIcon.src = ''; // You could set a default image
+        }
+
+        document.querySelector('.weather').style.display = 'block';
+        document.querySelector('.error').style.display = 'none';
     }
-            let data = await response.json()
-    
-
-
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
-
-    if(data.weather[0].main == 'Clouds'){
-        weatherIcon.src = 'images/clouds.png'
-    } 
-    else if(data.weather[0].main == 'Clear'){
-        weatherIcon.src = 'images/clear.png'
-    }
-     else if(data.weather[0].main == 'Rain'){
-        weatherIcon.src = 'images/rain.png'
-    }
-     else if (data.weather[0].main == 'Drizzle'){
-        weatherIcon.src = 'images/drizzle.png'
-    } 
-    else if(data.weather[0].main == 'Mist'){
-        weatherIcon.src = 'images/mist.png'
-    }
-
-
 }
 
-searchBtn.addEventListener('click',() =>{
-    checkWeather(searchBox.value)
-})
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+});
